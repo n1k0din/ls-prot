ad4ce45b01575f6b2c12a17bc90b6a6d1c6dc23c
Никита Галкин <nikita@lkds.ru>
2020-09-01 16:51:29 +0700

    1) добавляет макс. длину сегмента в approx 2) теперь approx возвращает список точек, а не одну

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from collections import namedtuple
 from datetime import datetime, timedelta
+import typing as t
 
 Point = namedtuple("Point", ["t", "f"])
+
+ROUND_TO_HOUR = {
+    "microsecond": 0,
+    "second": 0,
+    "minute": 0
+}
+
+ONE_HOUR = timedelta(hours=1)
 
 
 def td_minutes(td: timedelta) -> float:
     return td.seconds / 60
 
 
-def approx(p1: Point, p2: Point) -> Point:
+def approx(p1: Point, p2: Point, max_segment=120) -> t.List[Point]:
     """
     на входе данные за 7:24 и 8:15
     на выходе линейно приближенное значение в 8:00
@@ -16,22 +25,28 @@ def approx(p1: Point, p2: Point) -> Point:
     delta = p2.t - p1.t
     delta_minutes = td_minutes(delta)
 
-    if delta_minutes >= 60:
+    if delta_minutes >= max_segment:
         return None
 
     k = (p2.f - p1.f) / delta_minutes
 
-    t0 = p2.t.replace(second=0, microsecond=0, minute=0)
+    i = (p1.t + ONE_HOUR).replace(**ROUND_TO_HOUR)
 
-    res = k * td_minutes(t0 - p1.t) + p1.f
+    res = []
+
+    while i <= p2.t:
+        p0 = Point(i, k * td_minutes(i - p1.t) + p1.f)
+        res.append(p0)
+        i += ONE_HOUR
+
 
     return res
 
 
 def main():
     p1 = Point(datetime(2020, 9, 1, 7, 24), 4)
-    p2 = Point(datetime(2020, 9, 1, 8, 15), 14)
-    print(approx(p1, p2))
+    p2 = Point(datetime(2020, 9, 1, 9, 15), 14)
+    print(approx(p1, p2, max_segment=120))
 
 
 if __name__ == "__main__":
